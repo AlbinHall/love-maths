b4396d3b4532a928994943c5fe0506dcbb3a23ac
AlbinHall <albin-hall1@hotmail.com>
2022-10-10 14:01:23 +0000

    add function for button alert and random number generetor for the runGame function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,30 @@
+// Waits for site to load
+//on click function
+
+document.addEventListener("DOMContentLoaded", function(){
+    let buttons = this.getElementsByTagName("button");
+
+    for (let button of buttons) {
+        button.addEventListener('click', function() {
+            if (this.getAttribute("data-type") === "submit") {
+                alert("You clicked submit!");
+            } else {
+                let gameType = this.getAttribute("data-type");
+                alert(`You clicked ${gameType}`);
+            }
+        })
+    }
+})
+
+/**
+ * The main game loop that is called when the browser loads
+ * and when the users answer been submitted
+ */
 function runGame() {
 
+    // creates random number 1-25
+    let num1 = Math.floor(Math.random() * 25) + 1;
+    let num2 = Math.floor(Math.random() * 25) + 1;
 }
 
 function checkAnswer() {
